32a6c61ad9bfe2cf145410f97790e19f4edc5c95
Dolan Klocke <dolank16@gmail.comm>
2024-04-14 21:06:31 -0400

    updates to bim component

--- src/bim-components/ToDoCreator/index.ts
+++ src/bim-components/ToDoCreator/index.ts
@@ -1,16 +1,33 @@
 import  *  as OBC from "openbim-components"
 
-export class ToDoCreator extends OBC.Component<null> implements OBC.UI {
+interface ToDo {
+    description: string,
+    date: Date,
+    fragmentMap: OBC.FragmentIdMap
+}
+
+export class ToDoCreator extends OBC.Component<ToDo[]> implements OBC.UI {
     enabled: true;
     static uuid: string = "4e8e4fb2-9ae9-4ec3-9550-44115a03c98d" // this is only a class attribute, will not exist on instances
     private _components: OBC.Components
     uiElement = new OBC.UIElement<{activationButton: OBC.Button, todoList: OBC.FloatingWindow}>() // with this generic
     // we are basically telling typescript that the uiElement will have these to properties
+    private _list: ToDo[] = []
     constructor(components: OBC.Components) {
         super(components) // super runs the constructor method from the class we are extending from
         this._components = components
         this._components.tools.add(ToDoCreator.uuid, this)
         this.setUI()
+    }
+
+    async addToDo(description: string) {
+        const highlighter = await this._components.tools.get(OBC.FragmentHighlighter)
+        const todo: ToDo = {
+            description: description,
+            date: new Date(),
+            fragmentMap: highlighter.selection.select
+        }
+        this._list.push(todo)
     }
 
     private setUI() {
@@ -22,7 +39,7 @@ export class ToDoCreator extends OBC.Component<null> implements OBC.UI {
             form.visible = !form.visible
         })
         form.onAccept.add(() => {
-            
+            this.addToDo(todoDescriptionInput.value)
         })
         // form input
         const todoDescriptionInput = new OBC.TextArea(this._components)
@@ -30,22 +47,29 @@ export class ToDoCreator extends OBC.Component<null> implements OBC.UI {
         // adding an obc object to another simpleUIComponent object like OBC.Modal we use slots.content.addChild()
         // slots.centent will return another simpleUIComponent
         form.slots.content.addChild(todoDescriptionInput)
+        form.slots.content.get().style.padding = "20px"
+        form.slots.content.get().style.display = "flex"
+        form.slots.content.get().style.flexDirection = "column"
+        form.slots.content.get().style.rowGap = "20px"
 
         // creating button
         const activationButton = new OBC.Button(this._components)
         activationButton.materialIcon = "construction"
+
         // new todo button
         const newToDoBtn = new OBC.Button(this._components, {name: "Create"})
         activationButton.addChild(newToDoBtn)
         newToDoBtn.onClick.add(() => {
-            form.visible = !form.visible
+            form.visible = true
         })
+
         // todo list button
         const listToDoBtn = new OBC.Button(this._components, {name: "List"})
         activationButton.addChild(listToDoBtn)
         listToDoBtn.onClick.add(() => {
             todoList.visible = !todoList.visible
         })
+
         //Creating floating window
         const todoList = new OBC.FloatingWindow(this._components)
         this._components.ui.add(todoList)
@@ -54,18 +78,11 @@ export class ToDoCreator extends OBC.Component<null> implements OBC.UI {
         this.uiElement.set({activationButton, todoList})
     }
 
-    get(): null {
-        
+    get(): ToDo[] {
+        return this._list
     }
 
 }
-
-
-
-
-
-
-
 
 
 
