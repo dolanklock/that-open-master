6ea04458bdb383eeec4d4db5526518ed2d686337
Dolan Klocke <dolank16@gmail.comm>
2024-05-29 08:19:41 -0400

    updates to ai renderer

--- src/bim-components/AIRenderer/index.ts
+++ src/bim-components/AIRenderer/index.ts
@@ -3,7 +3,21 @@ import { v4 as uuidv4 } from 'uuid'
 import  *  as OBC from "openbim-components"
 import {LibraryUIComponent} from "./src/libraryUIComponent"
 
+// const processURL = "https://stablediffusionapi.com/api/v3/img2img";
+// const proxyUrl = "https://cors-anywhere.herokuapp.com/"; // Avoids CORS locally
+// const uploadUrl = "https://stablediffusionapi.com/api/v3/base64_crop";
+
+
+// TODO: check if fetch in _uploadRender method is failing or if its the test() fetch that is...
+
+
 export class StableDiffusionRender {
+    APIKey = "5Dc5hLuEiPd9ie3PKG6Tv51hXDLlhU52iTOwPhqL6FJZdj6OC5cCYrngMpEq"
+    proxyURL: string
+    uploadURL: string
+    processURL: string
+    private _components: OBC.Components
+
     settings = {
       prompt: "", // Indications to the AI engine
       negative_prompt: null, // Indications to be avoided by the AI engine
@@ -19,69 +33,133 @@ export class StableDiffusionRender {
       webhook: null,
       track_id: null,
     };
-    async render(APIKey: string, image: string) {
-      // api key shouldn't be in your code on production, but on an environment variable
-  
-      const proxyUrl = "https://cors-anywhere.herokuapp.com/"; // Avoids CORS locally
-      const uploadUrl = "https://stablediffusionapi.com/api/v3/base64_crop";
-      const processURL = "https://stablediffusionapi.com/api/v3/img2img";
-  
-      // Let's upload the render to stable diffusion
-      const url = proxyUrl + uploadUrl;
-      const crop = "false";
-  
-      const rawUploadResponse = await fetch(url, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ APIKey, image, crop }),
-      });
-  
-      const uploadResponse = await rawUploadResponse.json();
-  
-      if (!uploadResponse.link) {
-        throw new Error("There was a problem with the upload!");
-      }
-  
-      // Image uploaded! Now, let's process it:
-  
-      const uploadedImageURL = uploadResponse.link;
-      const params = {
-        APIKey,
-        init_image: uploadedImageURL,
-        ...this.settings,
-      };
-  
-      const rawResponse = await fetch(processURL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(params),
-      });
-  
-      const response = await rawResponse.json();
-  
-      if (response.status === "success") {
-        return response.output as string[];
-      } else {
-        throw new Error("Something went wrong rendering");
-      }
+
+    constructor(components: OBC.Components, proxyURL: string, uploadURL: string, processURL: string) {
+        this._components = components
+        this.processURL = processURL
+        this.proxyURL = proxyURL
+        this.uploadURL = uploadURL
+    }
+
+    private _takeScreenshot() {
+        const postproductionRenderer = this._components.renderer as OBC.PostproductionRenderer
+        console.log("HERE", postproductionRenderer)
+        postproductionRenderer.postproduction.composer.render()
+        const renderer = postproductionRenderer.get();
+        const image = renderer.domElement.toDataURL("image/png");
+        return image
+    }
+
+    private async _uploadRender(APIKey: string, image: string) {
+        // api key shouldn't be in your code on production, but on an environment variable  
+        // Let's upload the render to stable diffusion
+        const url = this.proxyURL + this.uploadURL;
+        const crop = "false";
+    
+        const rawUploadResponse = await fetch(url, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ APIKey, image, crop }),
+        });
+    
+        const uploadResponse = await rawUploadResponse.json();
+        
+        if (!uploadResponse.link) {
+            throw new Error("There was a problem with the upload!");
+        }
+        return uploadResponse.link
+    }
+    async render(APIKey: string) {
+        const image = this._takeScreenshot()
+        console.log("IMAGE", image)
+        const uploadedImageURL = this._uploadRender(this.APIKey, image)
+        const params = {
+            APIKey,
+            init_image: uploadedImageURL,
+            ...this.settings,
+        };
+        // Image uploaded! Now, let's process it:
+        const rawResponse = await fetch(this.processURL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(params),
+        });
+    
+        const response = await rawResponse.json();
+    
+        if (response.status === "success") {
+            return response.output as string[];
+        } else {
+            throw new Error("Something went wrong rendering");
+        }
+        
+    }
+    async test(APIKey: string) {
+        const image = this._takeScreenshot()
+        console.log("IMAGE", image)
+        const uploadedImageURL = this._uploadRender(this.APIKey, image)
+        console.log(image)
+        // 1. take capture of scene for sending to SD api
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "key": APIKey,
+            "prompt": "a cat sitting on a bench",
+            "negative_prompt": "bad quality",
+            "init_image": uploadedImageURL,
+            "width": "512",
+            "height": "512",
+            "samples": "1",
+            "temp": false,
+            "safety_checker": false,
+            "strength":0.7,
+            "seed": null,
+            "webhook": null,
+            "track_id": null,
+        });
+
+        const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow',
+        };
+        // const url = this.proxyURL + "https://modelslab.com/api/v6/realtime/img2img"
+        const url = "https://modelslab.com/api/v6/realtime/img2img"
+        fetch(url, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+        // 2. send captured image to SD for rendering
+
+        // 3. get response from SD with finished image
+
+        // 4. save image to libarary and display in UI
     }
   }
-
-
 
 export class AIRenderer extends OBC.Component<LibraryUIComponent> implements OBC.UI{
     enabled: boolean = true
     static uuid: string = uuidv4()
     private _components: OBC.Components
     private _APIKey: string
+    proxyURL: string
+    uploadURL: string
+    processURL: string
+    renderer: StableDiffusionRender
     uiElement = new OBC.UIElement<{activationBtn: OBC.Button, LibraryUIComponent: LibraryUIComponent}>()
 
-    constructor(components: OBC.Components, APIKey: string) {
+    constructor(components: OBC.Components, APIKey: string, proxyURL: string, uploadURL: string, processURL: string) {
         super(components)
         this._components = components
         this._components.tools.add(AIRenderer.uuid, this)
         this._APIKey = APIKey
         this._setUI()
+        this.processURL = processURL
+        this.proxyURL = proxyURL
+        this.uploadURL = uploadURL
+        this.renderer = new StableDiffusionRender(this._components, this.proxyURL, this.uploadURL, this.processURL)
     }
 
     private _setUI() {
@@ -101,6 +179,11 @@ export class AIRenderer extends OBC.Component<LibraryUIComponent> implements OBC
         })
         form.onAccept.add(async () => {
             form.visible = false
+            this.renderer.render(this._APIKey).then(() => {
+                console.log("finished render")
+            }).catch((err) => {
+                console.log("ERRRRR", err)
+            })
         })
         // form input
         const todoDescriptionInput = new OBC.TextArea(this._components)
@@ -129,42 +212,7 @@ export class AIRenderer extends OBC.Component<LibraryUIComponent> implements OBC
      * sends image to stable diffusion and fetches api and returns image generated
      */
     private async _renderImage() {
-        // 1. take capture of scene for sending to SD api
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-
-        const raw = JSON.stringify({
-            "key": this._APIKey,
-            "prompt": "a cat sitting on a bench",
-            "negative_prompt": "bad quality",
-            "init_image": "https://raw.githubusercontent.com/CompVis/stable-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo.png",
-            "width": "512",
-            "height": "512",
-            "samples": "1",
-            "temp": false,
-            "safety_checker": false,
-            "strength":0.7,
-            "seed": null,
-            "webhook": null,
-            "track_id": null
-        });
-
-        const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-        };
-
-        fetch("https://modelslab.com/api/v6/realtime/img2img", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-        // 2. send captured image to SD for rendering
-
-        // 3. get response from SD with finished image
-
-        // 4. save image to libarary and display in UI
+        
     }
 
     private _saveImage() {
@@ -175,8 +223,8 @@ export class AIRenderer extends OBC.Component<LibraryUIComponent> implements OBC
     dispose() {
     }
 
-    get() {
-    
+    get(): any {
+        return undefined
     }
 }
 
